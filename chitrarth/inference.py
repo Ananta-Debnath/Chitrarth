45ab3f58019f4c6ec5600e6e70102b0a4845d926
Ananta Debnath <ananta.deb2005@gmail.com>
2025-11-28 01:05:45 +0600

    list again

--- chitrarth/inference.py
+++ chitrarth/inference.py
@@ -97,7 +97,8 @@ def eval_model(tokenizer, model, image_processor, context_len, query, image_file
     if image_file:
         # image = load_image(image_file)
         image = Image.open(image_file).convert("RGB")
-        image_tensor = image_processor.preprocess(image, return_tensors='pt', do_resize=True, do_normalize=True)['pixel_values'].half().cuda()
+        images = [image]
+        image_tensor = image_processor.preprocess(images, return_tensors='pt', do_resize=True, do_normalize=True)['pixel_values'].half().cuda()
     else:
         image_tensor = None
         prompt = prompt.replace(DEFAULT_IMAGE_TOKEN + '\n', '')

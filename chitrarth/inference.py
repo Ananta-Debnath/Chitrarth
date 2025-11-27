230914a9b487e71a8ac910484f652f6a72802643
Ananta Debnath <ananta.deb2005@gmail.com>
2025-11-28 00:54:49 +0600

    make image a list

--- chitrarth/inference.py
+++ chitrarth/inference.py
@@ -96,7 +96,7 @@ def eval_model(tokenizer, model, image_processor, context_len, query, image_file
     # print("prompt ", {prompt})
     if image_file:
         image = load_image(image_file)
-        image_tensor = image_processor.preprocess(image, return_tensors='pt', padding=True, truncation=True)['pixel_values'].half().cuda()
+        image_tensor = image_processor.preprocess([image], return_tensors='pt', padding=True)['pixel_values'].half().cuda()
     else:
         image_tensor = None
         prompt = prompt.replace(DEFAULT_IMAGE_TOKEN + '\n', '')
